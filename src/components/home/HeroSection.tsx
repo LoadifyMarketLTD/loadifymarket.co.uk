fbf690fb28f6e6c5b75cab3f89899c3f2328f300
XDrive Logistics <dannycourierltd@gmail.com>
2026-03-19 22:55:59 +0000

    Add HeroSection component

---
+++ src/components/home/HeroSection.tsx
@@ -0,0 +1,12 @@
+import Button from '../ui/button';
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <div>
+            {/* Hero Section Content */}
+        </div>
+    );
+};
+
+export default HeroSection;
