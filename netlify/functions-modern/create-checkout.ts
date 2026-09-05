f44031bb37400329ba1d74f31ee7f9dcbad27236
XDrive Logistics <dannycourierltd@gmail.com>
2026-09-05 16:34:49 +0100

    Install PostgREST catch compatibility for checkout

--- netlify/functions-modern/create-checkout.ts
+++ netlify/functions-modern/create-checkout.ts
@@ -1,3 +1,7 @@
 import { handler } from '../functions/create-checkout';
 import { withLambda } from '../function-runtime/lambdaCompat';
+import { installPostgrestCatchCompat } from '../function-runtime/postgrestCatchCompat';
+
+installPostgrestCatchCompat();
+
 export default withLambda(handler);
