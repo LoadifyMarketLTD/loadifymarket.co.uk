8354948eb326f62456f6bdde88cdcf36212ababe
XDrive Logistics <dannycourierltd@gmail.com>
2026-09-05 16:34:57 +0100

    Install PostgREST catch compatibility for mobile payments

--- netlify/functions-modern/create-payment-intent.ts
+++ netlify/functions-modern/create-payment-intent.ts
@@ -1,3 +1,7 @@
 import { handler } from '../functions/create-payment-intent';
 import { withLambda } from '../function-runtime/lambdaCompat';
+import { installPostgrestCatchCompat } from '../function-runtime/postgrestCatchCompat';
+
+installPostgrestCatchCompat();
+
 export default withLambda(handler);
