5d3bb396c903ee5508d67a0743b31c8038868fe0
XDrive Logistics <dannycourierltd@gmail.com>
2026-09-05 01:47:59 +0100

    Revalidate full repository lint after fixes

--- scripts/netlify-lint-scope.sh
+++ scripts/netlify-lint-scope.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 set -eu
 
-find \
-  .github android artifacts docs playstore_screenshots public resources supabase \
-  -type f \( -name '*.ts' -o -name '*.tsx' \) -print0 \
-  | xargs -0 -r npx eslint
+npm run lint
