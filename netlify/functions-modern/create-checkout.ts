76c437c0cc6b6c6e02917d8e3ae69db74896b7ec
XDrive Logistics <dannycourierltd@gmail.com>
2026-09-05 21:28:07 +0100

    Return checkout responses with JSON content type

--- netlify/functions-modern/create-checkout.ts
+++ netlify/functions-modern/create-checkout.ts
@@ -1,8 +1,14 @@
 import { handler } from '../functions/create-checkout';
 import { withMarketplaceTaxEvidenceRepair } from '../functions/_shared/marketplaceTaxEvidenceRepair';
+import { ensureJsonContentType } from '../function-runtime/jsonContentTypeCompat';
 import { withLambda } from '../function-runtime/lambdaCompat';
 import { installPostgrestCatchCompat } from '../function-runtime/postgrestCatchCompat';
 
 installPostgrestCatchCompat();
 
-export default withLambda(withMarketplaceTaxEvidenceRepair(handler));
+const checkoutHandler = withLambda(withMarketplaceTaxEvidenceRepair(handler));
+
+export default async (request: Request, context: { requestId?: string }): Promise<Response> => {
+  const response = await checkoutHandler(request, context);
+  return ensureJsonContentType(response);
+};
