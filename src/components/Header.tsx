4739eb637e1c6170038cd66453925fa6ab363b4c
Ion Daniel Preda <dannycourierltd@gmail.com>
2026-09-25 11:51:43 +0100

    fix: remove unused header import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import { isCapacitorContext } from "@/lib/capacitorUtils";
 
